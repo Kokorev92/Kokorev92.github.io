00c1d10c55586fccfc5318fc4473062da4cacbab
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-10-05 10:54:14 +0300

    [feature]: добавил проект - калькулятор строк для LCD1602

---
+++ projects/lcd1602_calc.md
@@ -0,0 +1,6 @@
+---
+layout: post
+title: Калькулятор для LCD1602
+---
+
+{% include lcd1602.html %}
---
+++ static/js/lcd1602_script.js
@@ -0,0 +1,33 @@
+var string_1 = document.getElementById("str1");
+var string_2 = document.getElementById("str2");
+
+string_1.oninput = function () {
+  for (var i = 0; i < 16; i++) {
+    if (typeof string_1.value[i] == "undefined") {
+      document.getElementById((i + 1).toString()).innerHTML = "";
+    } else {
+      document.getElementById((i + 1).toString()).innerHTML = string_1.value[i];
+    }
+  }
+  if (string_1.value.length > 16) {
+    document.getElementById("first").style.backgroundColor = "red";
+  } else {
+    document.getElementById("first").style.backgroundColor = "aqua";
+  }
+};
+
+string_2.oninput = function () {
+  for (var i = 0; i < 16; i++) {
+    if (typeof string_2.value[i] == "undefined") {
+      document.getElementById((i + 20).toString()).innerHTML = "";
+    } else {
+      document.getElementById((i + 20).toString()).innerHTML =
+        string_2.value[i];
+    }
+  }
+  if (string_2.value.length > 16) {
+    document.getElementById("second").style.backgroundColor = "red";
+  } else {
+    document.getElementById("second").style.backgroundColor = "aqua";
+  }
+};
